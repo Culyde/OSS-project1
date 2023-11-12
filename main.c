aeaa28fd01caaa69b7aa43f0eaacbb91ff85986c
2023040024 <147572296+2023040024@users.noreply.github.com>
2023-11-12 22:03:26 +0900

    Update main.c
    콘솔 이동 함수 추가

--- main.c
+++ main.c
@@ -22,6 +22,13 @@ void titleDraw(){
 	printf("               #######      #    #      ##   ######       #######      ##         ##   #          #  #########\n");
 }
 
+void gotoxy(int x, int y){
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); // 콘솔 핸들가져오기
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
 
 int main()
 {
