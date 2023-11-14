0a6daa248e70f0be54c1beb2663e1b1a57c61859
Culyde <wnsdyd4418@naver.com>
2023-11-14 15:46:36 +0900

    no message

--- SoureTreeTest.c
+++ SoureTreeTest.c
@@ -1,7 +1,106 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define SUBMIT 4
+
+int titleDraw();
+int gotoxy();
+init();
+void menuDraw();
+int keyControl();
+
+void init() {
+	system("mode con cols=56 lines=20 | title DINOGAME");
+}
+
+void titleDraw() {
+	printf("\n\n\n");
+	printf("               #######      ##    ##      ##    ######        ######          #         ##        ##  #########\n");
+	printf("               ##     ##    ##    ## #    ##   ##    ##     ##     ##        ###        # #      # #  #\n");
+	printf("               ##      ##   ##    ##  #   ##   ##    ##    ##       ##      ## ##       #  #    #  #  #\n");
+	printf("               ##      ##   ##    ##   #  ##   ##    ##    ##              ##   ##      #   #  #   #  #########\n");
+	printf("               ##      ##   ##    ##   #  ##   ##    ##    ##    #####    #########     #    ##    #  #\n");
+	printf("               ##     ##    ##    ##    # ##   ##    ##     ##     ##    ##       ##    #          #  #\n");
+	printf("               #######      ##    ##     ###    ######       #######    ##         ##   #          #  #########\n");
+}
+
+void gotoxy(int x, int y) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE); // 콘솔 핸들가져오기
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
+void menuDraw() {
+	int x = 50, y = 20;
+	gotoxy(x, y);
+	printf("   > GAME START");
+	gotoxy(x, y + 1);
+	printf("     CHARCTERS");
+	gotoxy(x + 2, y + 2);
+	printf("    RECORDS");
+	gotoxy(x + 2, y + 3);
+	printf("   GAME RULE");
+	gotoxy(x + 2, y + 4);
+	printf("     EXIT");
+
+	while (1) {
+		int n = keyControl();
+		switch (n) {
+		case UP: {
+			if (y > 20) {
+				gotoxy(x + 3, y);
+				printf(" ");
+				gotoxy(x + 3, --y);
+				printf(">");
+			}
+			break;
+		}
+		case DOWN: {
+			if (y < 24) {
+				gotoxy(x + 3, y);
+				printf(" ");
+				gotoxy(x + 3, ++y);
+				printf(">");
+			}
+			break;
+		}
+		case SUBMIT: {
+			return y - 12;
+		}
+		}
+	}
+}
+
+int keyControl() { //키를 컨트롤하는 함수
+	char temp = getch();
+	if (temp == 'w' || temp == 'W') {
+		return UP;
+	}
+	else if (temp == 'a' || temp == 'A') {
+		return LEFT;
+	}
+	else if (temp == 's' || temp == 'S') {
+		return DOWN;
+	}
+	else if (temp == 'd' || temp == 'D') {
+		return RIGHT;
+	}
+	else if (temp == ' ') { // 엔터키가 선택버튼
+		return SUBMIT;
+	}
+}
+
 int main()
 {
-    printf("First OSS Code\n");
-
-    return 0;
+	titleDraw();
+	menuDraw();
+	return 0;
 }
